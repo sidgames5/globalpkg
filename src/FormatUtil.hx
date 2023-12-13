2500e9071d22975257b330f4e4d3d95837e84417
SidGames5 <saytleader@outlook.com>
2023-12-13 14:55:04 -0500

    get info on a package

--- src/FormatUtil.hx
+++ src/FormatUtil.hx
@@ -1,3 +1,7 @@
+import sys.io.File;
+
+using StringTools;
+
 enum FormatType {
 	ALPM;
 	OTHER;
@@ -6,5 +10,28 @@ enum FormatType {
 class FormatUtil {
 	public static function detectFormat(file:String):FormatType {
 		return OTHER;
+	}
+
+	public static function getInfo(file:String, format:FormatType):Int {
+		switch (format) {
+			case ALPM:
+				var decomp = Sys.command('tar -xf --zstd $file -C /tmp/globalpkg/$file');
+				if (decomp > 0) {
+					return 1;
+				}
+				final pkginfo = File.getContent('/tmp/globalpkg/$file/.PKGINFO');
+				for (line in pkginfo.split('\n')) {
+					if (line.startsWith("pkgname")) {
+						PkgInfo.name = line.substring(10);
+					}
+					if (line.startsWith("pkgver")) {
+						PkgInfo.version = line.substring(9);
+					}
+				}
+			case OTHER:
+				PkgInfo.name = "Unknown";
+				PkgInfo.version = "Unknown";
+		}
+		return 0;
 	}
 }
